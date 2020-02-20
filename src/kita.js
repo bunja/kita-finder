faa89b14b7a81340d89232441a2cc9eea212b0a2
Larisa Kosareva <kos.lv@yandex.com>
2020-02-20 23:34:43 +0100

    make kita form great again

--- src/kita.js
+++ src/kita.js
@@ -36,213 +36,153 @@ export default function Kita() {
     };
 
     return (
-        <div>
-            {!editMode && (
-                <div>
-                    <div className="kita-profile ">
-                        <img src="/kita.png" className="circular--square" />
-                        <div className="content post">
-                            <div className=" main">
-                                <div className="preview">
-                                    <img className="kita-pic" src="/logo.png" />
+        <div className="kita-profile findkita">
+            <img src="/kita.png" className="circular--square" />
+
+            <div className="findkita-container">
+                <div className="wrap">
+                    <div className="post main">
+                        <div className="pic-info">
+                            <img className="kita-pic" src="/logo.png" />
+                        </div>
+                        {!editMode && (
+                            <div className="kita-info mb-10">
+                                <div className="kita-info-row">
+                                    <span className="title">
+                                        {kita.kitaname}
+                                    </span>
+                                    <a href={"mailto:" + kita.email}>✉️</a>
+                                    &nbsp;
+                                    <a href={kita.web_site}>🌍</a>
                                 </div>
-                                <div className="main-info">
-                                    <div className="name">
-                                        <p className="kitaname">
-                                            {kita.kitaname}
-                                        </p>
-                                    </div>
-                                    <div className="address">
-                                        <span className="title">Address:</span>
-                                        <br /> Zip: {kita.zip_code}
-                                        <br />
-                                        City: {kita.city}
-                                        <br />
-                                        Street: {kita.street_hous}
-                                        <br />
-                                    </div>
-                                    <div className="contact-info">
-                                        <p>
-                                            Phone: {kita.phone_number}
-                                            <br />
-                                            email: {kita.email}
-                                        </p>
-                                    </div>
+                                <div className="kita-info-row">
+                                    Address: {kita.city}, {kita.street_hous},
+                                    {kita.zip_code}
                                 </div>
-                            </div>
-                            <div className=" sec">
-                                <div className="desc">
-                                    Some information:
+                                <div className="kita-info-row">
+                                    Open: {kita.time_of_work}, Age: {kita.age}
+                                </div>
+                                <div className="kita-info-row">
+                                    Information: {kita.num_of_places} places,{" "}
+                                    {kita.available} available
+                                </div>
+                                <div className="kita-info-row">
                                     <p>{kita.description}</p>
                                 </div>
-                                <div className="time-of-work">
-                                    {" "}
-                                    Open: {kita.time_of_work}{" "}
-                                </div>
-                                <div className="age">
-                                    {" "}
-                                    Age of kids: {kita.age}
-                                </div>
-                                <div className="website"> www.zandec.com</div>
-                            </div>
-
-                            <div className=" ter">
-                                <div className="application">Aplication:</div>
-                                <div className="places">
-                                    Places: {kita.num_of_places}
-                                </div>
-                                <div className="available">
-                                    Available: {kita.available}
-                                </div>
-                                <div className="kita-edit ">
+                                <div className="kita-info-row">
                                     <a href="#" onClick={e => editClick()}>
                                         Edit
                                     </a>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            )}
-
-            {editMode && (
-                <div className="kita-profile ">
-                    <img src="/kita.png" className="circular--square" />
-                    <div className="content post">
-                        <div className=" main">
-                            <div className="preview">
-                                <img className="kita-pic" src="/logo.png" />
-                            </div>
-                            <div className="main-info">
-                                <div className="name">
-                                    <p className="kitaname">
-                                        <input
-                                            name="kitaname"
-                                            defaultValue={kita.kitaname}
-                                            onChange={e => handleChange(e)}
-                                        />
-                                    </p>
+                        )}
+                        {editMode && (
+                            <div className="kita-info mb-10">
+                                <div className="kita-info-row">
+                                    Kita name:{" "}
+                                    <input
+                                        name="kitaname"
+                                        defaultValue={kita.kitaname}
+                                        onChange={e => handleChange(e)}
+                                        className="grey-input"
+                                    />
                                 </div>
-                                <div className="address">
-                                    <p>
-                                        <span className="title">Address:</span>
-                                        <br />
-                                        Zip:
-                                        <input
-                                            name="zip_code"
-                                            defaultValue={kita.zip_code}
-                                            onChange={e => handleChange(e)}
-                                        />
-                                        <br />
-                                        City:
-                                        <input
-                                            name="city"
-                                            defaultValue={kita.city}
-                                            onChange={e => handleChange(e)}
-                                        />
-                                        <br />
-                                        Street:
-                                        <input
-                                            name="street_hous"
-                                            defaultValue={kita.street_hous}
-                                            onChange={e => handleChange(e)}
-                                        />
-                                        <br />
-                                    </p>
+                                <div className="kita-info-row">
+                                    Email:{" "}
+                                    <input
+                                        name="email"
+                                        defaultValue={kita.email}
+                                        onChange={e => handleChange(e)}
+                                        className="grey-input"
+                                    />
+                                    , Website:
+                                    <input
+                                        name="web_site"
+                                        defaultValue={kita.web_site}
+                                        onChange={e => handleChange(e)}
+                                        className="grey-input"
+                                    />
                                 </div>
-                                <div className="contact-info">
-                                    <p>
-                                        Phone:{" "}
-                                        <input
-                                            name="phone_number"
-                                            defaultValue={kita.phone_number}
-                                            onChange={e => handleChange(e)}
-                                        />
-                                        <br />
-                                        email:{" "}
-                                        <input
-                                            name="email"
-                                            defaultValue={kita.email}
-                                            onChange={e => handleChange(e)}
-                                        />
-                                    </p>
+                                <div className="kita-info-row">
+                                    City:{" "}
+                                    <input
+                                        name="city"
+                                        defaultValue={kita.city}
+                                        onChange={e => handleChange(e)}
+                                        className="grey-input"
+                                    />
+                                    , Street:{" "}
+                                    <input
+                                        name="street_hous"
+                                        defaultValue={kita.street_hous}
+                                        onChange={e => handleChange(e)}
+                                        className="grey-input"
+                                    />
+                                    , Zip:{" "}
+                                    <input
+                                        name="zip_code"
+                                        defaultValue={kita.zip_code}
+                                        onChange={e => handleChange(e)}
+                                        className="w100px grey-input"
+                                    />
                                 </div>
-                            </div>
-                        </div>
-                        <div className=" sec">
-                            <div className="desc">
-                                Some information:
-                                <p>
+                                <div className="kita-info-row">
+                                    Open:{" "}
+                                    <input
+                                        name="time_of_work"
+                                        defaultValue={kita.time_of_work}
+                                        onChange={e => handleChange(e)}
+                                        className="w100px grey-input"
+                                    />
+                                    , Age:{" "}
+                                    <input
+                                        name="age"
+                                        defaultValue={kita.age}
+                                        onChange={e => handleChange(e)}
+                                        className="w100px grey-input"
+                                    />
+                                </div>
+                                <div className="kita-info-row">
+                                    Total number of places:{" "}
+                                    <input
+                                        name="num_of_places"
+                                        defaultValue={kita.num_of_places}
+                                        onChange={e => handleChange(e)}
+                                        className="w100px grey-input"
+                                    />
+                                    , Available:
+                                    <input
+                                        name="available"
+                                        defaultValue={kita.available}
+                                        onChange={e => handleChange(e)}
+                                        className="w100px grey-input"
+                                    />
+                                </div>
+                                <div className="kita-info-row">
                                     <textarea
                                         name="description"
                                         defaultValue={kita.description}
                                         onChange={e => handleChange(e)}
+                                        className="w100 grey-input"
                                     />
-                                </p>
+                                </div>
+                                <div className="kita-info-row">
+                                    <a
+                                        href="#"
+                                        onClick={e => {
+                                            handleSave();
+                                            setEditMode(false);
+                                            dispatch(updateKitaInfo(values));
+                                        }}
+                                    >
+                                        Save
+                                    </a>
+                                </div>
                             </div>
-                            <div className="time-of-work">
-                                {" "}
-                                Open:{" "}
-                                <input
-                                    name="time_of_work"
-                                    defaultValue={kita.time_of_work}
-                                    onChange={e => handleChange(e)}
-                                />{" "}
-                            </div>
-                            <div className="age">
-                                {" "}
-                                Age of kids:{" "}
-                                <input
-                                    name="age"
-                                    defaultValue={kita.age}
-                                    onChange={e => handleChange(e)}
-                                />
-                            </div>
-                            <div className="website">
-                                {" "}
-                                www.zandec.com
-                                <input
-                                    name="web_site"
-                                    defaultValue={kita.web_site}
-                                    onChange={e => handleChange(e)}
-                                />
-                            </div>
-                        </div>
-
-                        <div className=" ter">
-                            <div className="application">Aplication:</div>
-                            <div className="places">
-                                Places:&nbsp;&nbsp;
-                                <input
-                                    name="num_of_places"
-                                    defaultValue={kita.num_of_places}
-                                    onChange={e => handleChange(e)}
-                                />
-                            </div>
-                            <div className="available">
-                                Available:&nbsp;
-                                <input
-                                    name="available"
-                                    defaultValue={kita.available}
-                                    onChange={e => handleChange(e)}
-                                />
-                            </div>
-                            <div className="kita-save">
-                                <a
-                                    href="#"
-                                    onClick={e => {
-                                        handleSave();
-                                        setEditMode(false);
-                                        dispatch(updateKitaInfo(values));
-                                    }}
-                                >
-                                    Save
-                                </a>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div>
-            )}
+            </div>
         </div>
     );
 }
